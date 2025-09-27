e34373f68f024e82f430c5892ffe3213644e999e
Gregory Halverson <gregory.halverson@gmail.com>
2025-09-26 19:11:03 -0700

    getting S3/Planetary back-end working

--- LandsatL2C2/__init__.py
+++ LandsatL2C2/__init__.py
@@ -1,10 +1,5 @@
-from os.path import join, abspath, dirname
-
 from .LandsatL2C2 import *
 from .backends import LandsatBackend, M2MBackend, S3Backend, create_backend
+from .version import __version__
 
-with open(join(abspath(dirname(__file__)), "version.txt")) as f:
-    version = f.read()
-
-__version__ = version
 __author__ = "Gregory H. Halverson"
